9583380b63d50a3a47edb092d1d495d4125c5571
pepu <pepshove@gmail.com>
2025-08-19 22:18:52 +0200

    first commit

---
+++ Env/dev/main.tf
@@ -0,0 +1,20 @@
+locals {
+    instance_name = "${terraform.workspace}-instance"
+}
+provider "aws" {
+    region = var.aws_region
+
+}
+resource "aws_instance" "web" {
+   
+        ami                         = var.aws_ami
+        instance_type               = var.instance_type
+        key_name                    = var.key_pair
+        subnet_id                   = var.subnet_id
+        vpc_security_group_ids      = var.vpc_security_group_ids
+    
+    tags = {
+        Name = local.instance_name
+    }
+}
+  

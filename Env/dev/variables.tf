c6aefb01e319f5324514d76721bd4546d816115e
pepu <pepshove@gmail.com>
2025-08-20 21:58:38 +0200

    commited to dev-env

---
+++ Env/dev/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+    type   = string
+}
+variable "aws_ami" {
+    type    = string
+    
+}
+
+variable "instance_type" {
+    type    = string
+
+}
+
+variable "key_pair" {
+    type    = string 
+}
+
+variable "vpc_security_group_ids" {
+    type    = list(string)
+    sensitive = true
+  
+}
+
+variable "subnet_id" {
+    type    = string
+    sensitive = true
+}
+

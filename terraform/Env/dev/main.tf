bc7de3b543d46e073ee8c27139687004e35f2f9b
pepu <pepshove@gmail.com>
2025-08-21 12:45:26 +0200

    dev-env

--- terraform/Env/dev/main.tf
+++ terraform/Env/dev/main.tf
@@ -6,14 +6,18 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "web" {
-  ami                    = var.aws_ami
-  instance_type          = var.instance_type
-  key_name               = var.key_pair
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-
-  tags = {
-    Name = local.instance_name
-  }
+module "compute" {
+  source = "../../modules/compute"
 }
+# resource "aws_instance" "web" {
+  
+#   ami                    = var.aws_ami
+#   instance_type          = var.instance_type
+#   key_name               = var.key_pair
+#   subnet_id              = var.subnet_id
+#   vpc_security_group_ids = var.vpc_security_group_ids
+
+#   tags = {
+#     Name = local.instance_name
+#   }
+# }
